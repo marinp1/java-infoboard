77392e72e677172321023dc96fd766d3b3816ca6
Patrik Marin <patrik.marin@gmail.com>
2016-10-23 17:33:08 +0300

    Initalised soon-to-be settings window and added spacing to eather icons if multiple are to be displayed.

--- src/main/java/fi/patrikmarin/infoboard/utils/Utils.java
+++ src/main/java/fi/patrikmarin/infoboard/utils/Utils.java
@@ -36,58 +36,58 @@ public class Utils {
 		weatherMap.put("2_2", "\uf086");
 		
 		weatherMap.put("21_1", "\uf009");
-		weatherMap.put("22_1", "\uf009\uf009");
-		weatherMap.put("23_1", "\uf009\uf009\uf009");
+		weatherMap.put("22_1", "\uf009 \uf009");
+		weatherMap.put("23_1", "\uf009 \uf009 \uf009");
 		weatherMap.put("21_2", "\uf029");
-		weatherMap.put("22_2", "\uf029\uf029");
-		weatherMap.put("23_2", "\uf029\uf029\uf029");
+		weatherMap.put("22_2", "\uf029 \uf029");
+		weatherMap.put("23_2", "\uf029 \uf029 \uf029");
 		
 		weatherMap.put("3_1", "\uf013");
 		weatherMap.put("31_1", "\uf019");
-		weatherMap.put("32_1", "\uf019\uf019");
-		weatherMap.put("33_1", "\uf019\uf019\uf019");
+		weatherMap.put("32_1", "\uf019 \uf019");
+		weatherMap.put("33_1", "\uf019 \uf019 \uf019");
 		weatherMap.put("3_2", "\uf013");
 		weatherMap.put("31_2", "\uf019");
-		weatherMap.put("32_2", "\uf019\uf019");
-		weatherMap.put("33_2", "\uf019\uf019\uf019");
+		weatherMap.put("32_2", "\uf019 \uf019");
+		weatherMap.put("33_2", "\uf019 \uf019 \uf019");
 		
 		weatherMap.put("41_1", "\uf00a");
-		weatherMap.put("42_1", "\uf00a\uf00a");
-		weatherMap.put("43_1", "\uf00a\uf00a\uf00a");
+		weatherMap.put("42_1", "\uf00a \uf00a");
+		weatherMap.put("43_1", "\uf00a \uf00a \uf00a");
 		weatherMap.put("41_2", "\uf02a");
-		weatherMap.put("42_2", "\uf02a\uf02a");
-		weatherMap.put("43_2", "\uf02a\uf02a\uf02a");
+		weatherMap.put("42_2", "\uf02a \uf02a");
+		weatherMap.put("43_2", "\uf02a \uf02a \uf02a");
 		
 		weatherMap.put("51_1", "\uf01b");
-		weatherMap.put("52_1", "\uf01b\uf01b");
-		weatherMap.put("53_1", "\uf01b\uf01b\uf01b");
+		weatherMap.put("52_1", "\uf01b \uf01b");
+		weatherMap.put("53_1", "\uf01b \uf01b \uf01b");
 		weatherMap.put("51_2", "\uf01b");
-		weatherMap.put("52_2", "\uf01b\uf01b");
-		weatherMap.put("53_2", "\uf01b\uf01b\uf01b");
+		weatherMap.put("52_2", "\uf01b \uf01b");
+		weatherMap.put("53_2", "\uf01b \uf01b \uf01b");
 		
 		weatherMap.put("61_1", "\uf00e");
-		weatherMap.put("62_1", "\uf00e\uf00e");
+		weatherMap.put("62_1", "\uf00e \uf00e");
 		weatherMap.put("61_2", "\uf02c");
-		weatherMap.put("62_2", "\uf02c\uf02c");
+		weatherMap.put("62_2", "\uf02c \uf02c");
 		
 		weatherMap.put("63_1", "\uf01e");
-		weatherMap.put("64_1", "\uf01e\uf01e");	
+		weatherMap.put("64_1", "\uf01e \uf01e");	
 		weatherMap.put("63_2", "\uf01e");
-		weatherMap.put("64_2", "\uf01e\uf01e");	
+		weatherMap.put("64_2", "\uf01e \uf01e");	
 		
 		weatherMap.put("71_1", "\uf006");
-		weatherMap.put("72_1", "\uf006\uf006");
-		weatherMap.put("73_1", "\uf006\uf006\uf006");
+		weatherMap.put("72_1", "\uf006 \uf006");
+		weatherMap.put("73_1", "\uf006 \uf006 \uf006");
 		weatherMap.put("71_2", "\uf034");
-		weatherMap.put("72_2", "\uf034\uf034");
-		weatherMap.put("73_2", "\uf034\uf034\uf034");
+		weatherMap.put("72_2", "\uf034 \uf034");
+		weatherMap.put("73_2", "\uf034 \uf034 \uf034");
 		
 		weatherMap.put("81_1", "\uf017");
-		weatherMap.put("82_1", "\uf017\uf017");
-		weatherMap.put("83_1", "\uf017\uf017");
+		weatherMap.put("82_1", "\uf017 \uf017");
+		weatherMap.put("83_1", "\uf017 \uf017");
 		weatherMap.put("81_2", "\uf017");
-		weatherMap.put("82_2", "\uf017\uf017");
-		weatherMap.put("83_2", "\uf017\uf017");
+		weatherMap.put("82_2", "\uf017 \uf017");
+		weatherMap.put("83_2", "\uf017 \uf017");
 		
 		weatherMap.put("91_1", "\uf0b6");
 		weatherMap.put("92_2", "\uf003");
